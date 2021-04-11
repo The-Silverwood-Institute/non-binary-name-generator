ab8b0b74eb801b82a7260c9a329921281036a7d5
Kit <kittsville@googlemail.com>
2021-04-11 19:17:50 +0100

    Disallow vowel-less names

--- generate.py
+++ generate.py
@@ -6,17 +6,18 @@ with open('names.txt') as names_file:
     names = [name.strip().lower() for name in names_file.readlines()]
 
 alphabet = list(string.ascii_lowercase)
+vowels = set('aeiouy')
 frequency = {}
 
 for letter in alphabet:
     frequency[letter] = {}
 
     for next_letter in alphabet:
-        frequency[letter][next_letter] = 0
+        frequency[letter][next_letter] = 1
 
 for name in names:
-    frequency[name[0]][name[1]] += 1
-    frequency[name[1]][name[2]] += 1
+    frequency[name[0]][name[1]] += 2
+    frequency[name[1]][name[2]] += 2
 
 def next_letter(letter):
     return random.choices(list(frequency[letter].keys()), frequency[letter].values())[0]
@@ -26,4 +27,8 @@ for _ in range(0, 30):
     first_letter = random.choice(alphabet)
     second_letter = next_letter(first_letter)
     third_letter  = next_letter(second_letter)
+
+    if len(set([first_letter, second_letter, third_letter]).intersection(vowels)) == 0:
+        second_letter = random.choice(list(vowels))
+
     print(first_letter.upper() + second_letter + third_letter)
